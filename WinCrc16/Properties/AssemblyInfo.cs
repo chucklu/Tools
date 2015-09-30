d713236fb37711efffa7fcf15f395e13a41e4330
ChuckLu <244657538@qq.com>
2015-09-30 13:41:12 +0800

    修改AssemblyTitle和AssemblyProduct为WinCrc16 将程序附加到开始菜单的时候，显示的是AssemblyTitle

--- WinCrc16/Properties/AssemblyInfo.cs
+++ WinCrc16/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Crc16")]
+[assembly: AssemblyTitle("WinCrc16")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Crc16")]
+[assembly: AssemblyProduct("WinCrc16")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
