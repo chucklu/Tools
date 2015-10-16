35d49b2cb33552ec3836a2af97a711b7941cecc0
ChuckLu <244657538@qq.com>
2015-10-16 16:45:22 +0800

    把时间转换为Unix时间戳对应的秒数

---
+++ UnixTime/FormMain.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace UnixTime
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.buttonConfirm = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(25, 71);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox.Size = new System.Drawing.Size(424, 175);
+            this.textBox.TabIndex = 0;
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker.Location = new System.Drawing.Point(25, 24);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(200, 21);
+            this.dateTimePicker.TabIndex = 1;
+            // 
+            // buttonConfirm
+            // 
+            this.buttonConfirm.Location = new System.Drawing.Point(259, 25);
+            this.buttonConfirm.Name = "buttonConfirm";
+            this.buttonConfirm.Size = new System.Drawing.Size(75, 23);
+            this.buttonConfirm.TabIndex = 2;
+            this.buttonConfirm.Text = "确定";
+            this.buttonConfirm.UseVisualStyleBackColor = true;
+            this.buttonConfirm.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(374, 24);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 3;
+            this.buttonClear.Text = "清屏";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 293);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonConfirm);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.textBox);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Unix时间戳转换";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Button buttonConfirm;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ UnixTime/FormMain.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UnixTime
+{
+    public partial class FormMain : Form
+    {
+
+        private DateTime dateTime1970;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dateTime1970 = new DateTime(1970, 1, 1, 0, 0, 0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var timeSpan = dateTimePicker.Value - dateTime1970;
+            var seconds = timeSpan.Ticks / TimeSpan.TicksPerSecond;
+            textBox.AppendText(string.Format("时间:{3}  秒数:{0}  0x{1}{2}",
+                seconds, seconds.ToString("X2"), Environment.NewLine,dateTimePicker.Value.ToString("yyyy-MM-dd HH:mm:ss")));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox.Clear();
+        }
+    }
+}
