49d0a7a7b1d55fe12a0926b9de24d9e78b4fd2cd
ChuckLu <244657538@qq.com>
2015-10-17 20:44:51 +0800

    添加了火车票购买时间查询的项目

---
+++ TrainTicket/FormMain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TrainTicket
+{
+    public partial class FormMain : Form
+    {
+        /// <summary>
+        /// 可以提前60天购票
+        /// </summary>
+        private const int AheadDays = 60;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            LoadBuyTicketTime();
+        }
+
+        private void LoadBuyTicketTime()
+        {
+            DateTime dateTime = DateTime.Now;
+            //因为当天买票坐火车，也算是提前一天买票了，所这里加上提前的天数-1
+            var buyTicketTime = dateTime.AddDays(AheadDays - 1);
+            textBox1.AppendText(string.Format("今天是{0},{2}可以买到{1}的火车票",
+                dateTime.ToString("yyyy年MM月dd日"), buyTicketTime.ToString("yyyy年MM月dd日"), Environment.NewLine));
+        }
+    }
+}
