4cb901102274d1a9a9ee36432de2d0ef6d72c2a0
ChuckLu <244657538@qq.com>
2015-11-04 10:31:13 +0800

    基于Unix时间的秒数转换，改为基于基准时间的转换 设定的基准时间可以保存在配置文件中

--- UnixTime/FormMain.Designer.cs
+++ UnixTime/FormMain.Designer.cs
@@ -29,18 +29,20 @@
         private void InitializeComponent()
         {
             this.textBoxTimeToSeconds = new System.Windows.Forms.TextBox();
-            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerCurrent = new System.Windows.Forms.DateTimePicker();
             this.buttonTimeToSeconds = new System.Windows.Forms.Button();
             this.buttonClearSeconds = new System.Windows.Forms.Button();
             this.textBoxSeconds = new System.Windows.Forms.TextBox();
             this.buttonSecondsToTime = new System.Windows.Forms.Button();
             this.textBoxSecondsToTime = new System.Windows.Forms.TextBox();
             this.buttonClearTime = new System.Windows.Forms.Button();
+            this.dateTimePickerBasic = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBoxTimeToSeconds
             // 
-            this.textBoxTimeToSeconds.Location = new System.Drawing.Point(25, 71);
+            this.textBoxTimeToSeconds.Location = new System.Drawing.Point(23, 94);
             this.textBoxTimeToSeconds.Multiline = true;
             this.textBoxTimeToSeconds.Name = "textBoxTimeToSeconds";
             this.textBoxTimeToSeconds.ReadOnly = true;
@@ -48,18 +50,18 @@
             this.textBoxTimeToSeconds.Size = new System.Drawing.Size(424, 115);
             this.textBoxTimeToSeconds.TabIndex = 0;
             // 
-            // dateTimePicker
+            // dateTimePickerCurrent
             // 
-            this.dateTimePicker.CustomFormat = "yyyy-MM-dd HH:mm:ss";
-            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker.Location = new System.Drawing.Point(25, 24);
-            this.dateTimePicker.Name = "dateTimePicker";
-            this.dateTimePicker.Size = new System.Drawing.Size(200, 21);
-            this.dateTimePicker.TabIndex = 1;
+            this.dateTimePickerCurrent.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.dateTimePickerCurrent.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePickerCurrent.Location = new System.Drawing.Point(23, 47);
+            this.dateTimePickerCurrent.Name = "dateTimePickerCurrent";
+            this.dateTimePickerCurrent.Size = new System.Drawing.Size(200, 21);
+            this.dateTimePickerCurrent.TabIndex = 1;
             // 
             // buttonTimeToSeconds
             // 
-            this.buttonTimeToSeconds.Location = new System.Drawing.Point(259, 25);
+            this.buttonTimeToSeconds.Location = new System.Drawing.Point(257, 48);
             this.buttonTimeToSeconds.Name = "buttonTimeToSeconds";
             this.buttonTimeToSeconds.Size = new System.Drawing.Size(75, 23);
             this.buttonTimeToSeconds.TabIndex = 2;
@@ -69,24 +71,24 @@
             // 
             // buttonClearSeconds
             // 
-            this.buttonClearSeconds.Location = new System.Drawing.Point(374, 24);
+            this.buttonClearSeconds.Location = new System.Drawing.Point(372, 47);
             this.buttonClearSeconds.Name = "buttonClearSeconds";
             this.buttonClearSeconds.Size = new System.Drawing.Size(75, 23);
             this.buttonClearSeconds.TabIndex = 3;
             this.buttonClearSeconds.Text = "清屏";
             this.buttonClearSeconds.UseVisualStyleBackColor = true;
-            this.buttonClearSeconds.Click += new System.EventHandler(this.button2_Click);
+            this.buttonClearSeconds.Click += new System.EventHandler(this.buttonClearSeconds_Click);
             // 
             // textBoxSeconds
             // 
-            this.textBoxSeconds.Location = new System.Drawing.Point(25, 222);
+            this.textBoxSeconds.Location = new System.Drawing.Point(23, 245);
             this.textBoxSeconds.Name = "textBoxSeconds";
             this.textBoxSeconds.Size = new System.Drawing.Size(200, 21);
             this.textBoxSeconds.TabIndex = 4;
             // 
             // buttonSecondsToTime
             // 
-            this.buttonSecondsToTime.Location = new System.Drawing.Point(248, 222);
+            this.buttonSecondsToTime.Location = new System.Drawing.Point(246, 245);
             this.buttonSecondsToTime.Name = "buttonSecondsToTime";
             this.buttonSecondsToTime.Size = new System.Drawing.Size(75, 23);
             this.buttonSecondsToTime.TabIndex = 5;
@@ -96,7 +98,7 @@
             // 
             // textBoxSecondsToTime
             // 
-            this.textBoxSecondsToTime.Location = new System.Drawing.Point(25, 264);
+            this.textBoxSecondsToTime.Location = new System.Drawing.Point(23, 287);
             this.textBoxSecondsToTime.Multiline = true;
             this.textBoxSecondsToTime.Name = "textBoxSecondsToTime";
             this.textBoxSecondsToTime.ReadOnly = true;
@@ -105,7 +107,7 @@
             // 
             // buttonClearTime
             // 
-            this.buttonClearTime.Location = new System.Drawing.Point(374, 222);
+            this.buttonClearTime.Location = new System.Drawing.Point(372, 245);
             this.buttonClearTime.Name = "buttonClearTime";
             this.buttonClearTime.Size = new System.Drawing.Size(75, 23);
             this.buttonClearTime.TabIndex = 7;
@@ -113,22 +115,43 @@
             this.buttonClearTime.UseVisualStyleBackColor = true;
             this.buttonClearTime.Click += new System.EventHandler(this.buttonClearTime_Click);
             // 
+            // dateTimePickerBasic
+            // 
+            this.dateTimePickerBasic.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.dateTimePickerBasic.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePickerBasic.Location = new System.Drawing.Point(91, 12);
+            this.dateTimePickerBasic.Name = "dateTimePickerBasic";
+            this.dateTimePickerBasic.Size = new System.Drawing.Size(200, 21);
+            this.dateTimePickerBasic.TabIndex = 8;
+            this.dateTimePickerBasic.Value = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "基准时间:";
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(502, 393);
+            this.ClientSize = new System.Drawing.Size(502, 410);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePickerBasic);
             this.Controls.Add(this.buttonClearTime);
             this.Controls.Add(this.textBoxSecondsToTime);
             this.Controls.Add(this.buttonSecondsToTime);
             this.Controls.Add(this.textBoxSeconds);
             this.Controls.Add(this.buttonClearSeconds);
             this.Controls.Add(this.buttonTimeToSeconds);
-            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.dateTimePickerCurrent);
             this.Controls.Add(this.textBoxTimeToSeconds);
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Unix时间戳转换";
+            this.Text = "时间戳转换";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -138,13 +161,15 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBoxTimeToSeconds;
-        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.DateTimePicker dateTimePickerCurrent;
         private System.Windows.Forms.Button buttonTimeToSeconds;
         private System.Windows.Forms.Button buttonClearSeconds;
         private System.Windows.Forms.TextBox textBoxSeconds;
         private System.Windows.Forms.Button buttonSecondsToTime;
         private System.Windows.Forms.TextBox textBoxSecondsToTime;
         private System.Windows.Forms.Button buttonClearTime;
+        private System.Windows.Forms.DateTimePicker dateTimePickerBasic;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- UnixTime/FormMain.cs
+++ UnixTime/FormMain.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Windows.Forms;
+using UnixTime.Properties;
 
 namespace UnixTime
 {
     public partial class FormMain : Form
     {
 
-        private DateTime dateTime1970;
+        private DateTime dateTimeBasic = DateTime.MinValue;
 
         public FormMain()
         {
@@ -15,24 +16,37 @@ namespace UnixTime
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            dateTime1970 = new DateTime(1970, 1, 1, 0, 0, 0);
+            GetBasicTimeFromSetting();
+            dateTimePickerBasic.Value = dateTimeBasic;
         }
 
+        /// <summary>
+        /// 时间转换成相对于基准时间的秒数
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonTimeToSeconds_Click(object sender, EventArgs e)
         {
-            var timeSpan = dateTimePicker.Value - dateTime1970;
+            AppendBasicTime();
+            var timeSpan = dateTimePickerCurrent.Value - dateTimeBasic;
             var seconds = timeSpan.Ticks / TimeSpan.TicksPerSecond;
             textBoxTimeToSeconds.AppendText(string.Format("时间:{3}  秒数:{0}  0x{1}{2}",
-                seconds, seconds.ToString("X2"), Environment.NewLine,dateTimePicker.Value.ToString("yyyy-MM-dd HH:mm:ss")));
+                seconds, seconds.ToString("X2"), Environment.NewLine,dateTimePickerCurrent.Value.ToString("yyyy-MM-dd HH:mm:ss")));
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void buttonClearSeconds_Click(object sender, EventArgs e)
         {
             textBoxTimeToSeconds.Clear();
         }
 
+        /// <summary>
+        /// 相对于基准时间的描述转换为时间
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonSecondsToTime_Click(object sender, EventArgs e)
         {
+            AppendBasicTime();
             string sec = textBoxSeconds.Text.Trim();
             long seconds;
             if (sec.StartsWith("0x") || sec.StartsWith("0X"))
@@ -43,7 +57,7 @@ namespace UnixTime
             {
                 seconds = Convert.ToInt64(sec);
             }
-            var dateTime = dateTime1970.AddSeconds(seconds);
+            var dateTime = dateTimeBasic.AddSeconds(seconds);
             textBoxSecondsToTime.AppendText(string.Format("秒数:{0}  0x{1}  时间:{2}{3}",
                 seconds,seconds.ToString("X2"),dateTime.ToString("yyyy-MM-dd HH:mm:ss"),Environment.NewLine));
         }
@@ -51,6 +65,29 @@ namespace UnixTime
         private void buttonClearTime_Click(object sender, EventArgs e)
         {
             textBoxSecondsToTime.Clear();
+        }
+
+        private void AppendBasicTime()
+        {
+            if (dateTimeBasic != dateTimePickerBasic.Value)
+            {
+                SaveBasicTimeToSetting();
+                GetBasicTimeFromSetting();
+                string info = string.Format("{0}基准时间:{1}{0}", Environment.NewLine, dateTimePickerBasic.Value.ToString("yyyy-MM-dd HH:mm:ss"));
+                textBoxTimeToSeconds.AppendText(info);
+                textBoxSecondsToTime.AppendText(info);
+            }
+        }
+
+        private void GetBasicTimeFromSetting()
+        {
+            dateTimeBasic = Settings.Default.UnixTime;
+        }
+
+        private void SaveBasicTimeToSetting()
+        {
+            Settings.Default.UnixTime = dateTimePickerBasic.Value;
+            Settings.Default.Save();
         }
     }
 }
