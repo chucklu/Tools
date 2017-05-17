0b3e9e67efd28e88c265063a477448f69e3a68f3
Chuck Lu <244657538@qq.com>
2017-05-17 20:43:35 +0800

    filter the input for crc16(Modbus)

--- Encryptor/FormWinCrc16.cs
+++ Encryptor/FormWinCrc16.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
+using ChuckLu.Utility;
 
 namespace Encryptor
 {
@@ -12,8 +14,19 @@ namespace Encryptor
 
         private void buttonCrc16_Click(object sender, EventArgs e)
         {
+            string input = textBoxInput.Text;
+            //Check the input first
+            string pattern = @"^[0-9a-fA-F]$";
+            Regex regex = new Regex(pattern);
+            var match = regex.IsMatch(input);
+            if (!match)
+            {
+                ChuckMessageBox.ShowInfo("Please input the correct format of Hex");
+                return;
+            }
+
             //首先去除输入字符串中的空格
-            string input = textBoxInput.Text.Replace(" ", string.Empty);
+            input = input.Replace(" ", string.Empty);
             string result = ChuckLu.Utility.Crc16.CheckHexSpaceString(input);
             textBoxResult.Text = result;
         }
